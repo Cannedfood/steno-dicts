c42f5fd88f9c0ff92d83ba0f280769322d8759f2
Benno Straub <benno.straub@outlook.de>
2025-04-10 21:39:44 +0200

    Fix script on linux

--- generate.mjs
+++ generate.mjs
@@ -1,82 +1,91 @@
-#!node
+#!/bin/node
 
-import fs from 'fs/promises';
+import fs from "fs/promises";
 
-await fs.mkdir('benno-steno-dicts', { recursive: true });
+await fs.mkdir("benno-steno-dicts", { recursive: true });
 
 const movement = {
-	// Arrow keys
-	"#TPH-R": "{#Left}{^}",
-	"#TPHER": "{#Backspace}{^}",
+  // Arrow keys
+  "#TPH-R": "{#Left}{^}",
+  "#TPHER": "{#Backspace}{^}",
 
-	"#TPH-G": "{#Right}{^}",
-	"#TPHEG": "{#Delete}{^}",
+  "#TPH-G": "{#Right}{^}",
+  "#TPHEG": "{#Delete}{^}",
 
-	"#TPH-P": "{#Up}{^}",
-	"#TPHEP": "{#Control_L(Shift_L(k))}{#Up}{^}",
+  "#TPH-P": "{#Up}{^}",
+  "#TPHEP": "{#Control_L(Shift_L(k))}{#Up}{^}",
 
-	"#TPH-B": "{#Down}{^}",
-	"#TPHEB": "{#Control_L(Shift_L(k))}{^}",
+  "#TPH-B": "{#Down}{^}",
+  "#TPHEB": "{#Control_L(Shift_L(k))}{^}",
 
-	// Entire words
-	"#TPH-RB": "{#Control_L(Left)}{^}",
-	"#TPHERB": "{#Control_L(Backspace)}{^}",
-	"#TPH-BG": "{#Control_L(Right)}{^}",
-	"#TPHEBG": "{#Control_L(Delete)}{^}",
+  // Entire words
+  "#TPH-RB": "{#Control_L(Left)}{^}",
+  "#TPHERB": "{#Control_L(Backspace)}{^}",
+  "#TPH-BG": "{#Control_L(Right)}{^}",
+  "#TPHEBG": "{#Control_L(Delete)}{^}",
 
-	// End of line/Start of line
-	"#TPH-FR": "{#Home}{^}",
-	"#TPHEFR": "{#Shift_L(Home)}{#Backspace}{^}",
-	"#TPH-LG": "{#End}{^}",
-	"#TPHELG": "{#Shift_L(End)}{#Delete}{^}",
+  // End of line/Start of line
+  "#TPH-FR": "{#Home}{^}",
+  "#TPHEFR": "{#Shift_L(Home)}{#Backspace}{^}",
+  "#TPH-LG": "{#End}{^}",
+  "#TPHELG": "{#Shift_L(End)}{#Delete}{^}",
 };
 
 // Generate select stuff (basically just add shift if U is pressed)
 const movement2 = {};
 for (const [key, value] of Object.entries(movement)) {
-	movement2[key] = value;
+  movement2[key] = value;
 
-	if (!key.includes('-'))
-		continue;
+  if (!key.includes("-")) continue;
 
-	movement2[key.replace('-', 'U')] = 
-		value
-		.replace('{#', '{#Shift_L(')
-		.replace('}{^}', ')}{^}');
-	
-	movement2[key.replace('-', 'EU')] =
-		value
-		.replace('{#', '{#Alt_L(Shift_L(')
-		.replace('}{^}', '))}{^}');
+  movement2[key.replace("-", "U")] = value
+    .replace("{#", "{#Shift_L(")
+    .replace("}{^}", ")}{^}");
+
+  movement2[key.replace("-", "EU")] = value
+    .replace("{#", "{#Alt_L(Shift_L(")
+    .replace("}{^}", "))}{^}");
 }
 
 // Generate repetitions
 const movement3 = {};
 for (const [key, value] of Object.entries(movement2)) {
-	movement3[key] = value;
-	movement3[key + 'S'] = `${value}${value}`;
-	movement3[key + 'T'] = `${value}${value}${value}`;
-	movement3[key + 'TS'] = `${value}${value}${value}${value}`;
+  movement3[key] = value;
+  movement3[key + "S"] = `${value}${value}`;
+  movement3[key + "T"] = `${value}${value}${value}`;
+  movement3[key + "TS"] = `${value}${value}${value}${value}`;
 }
 
-await fs.writeFile('benno-steno-dicts/movement.json', JSON.stringify(movement3, null, '\t'), 'utf-8')
+await fs.writeFile(
+  "benno-steno-dicts/movement.json",
+  JSON.stringify(movement3, null, "\t"),
+  "utf-8",
+);
 
 const casing = {
-	// "NK" -> "normal case"
-	"TPH-BG": "{:set_space: }{:set_case:normal}",
-	// "TK" -> "Title Case"
-	"T-BG": "{:set_case:title}{:set_space:}",
-	// "PK" -> "PascalCase"
-	"P-BG": "{:set_case:title}{:set_space:}",
-	// "CK" -> "camelCase"
-	"KR-BG": "{:set_case:title}{:set_space:}{>}",
-	// "SK" -> "snake_case"
-	"S-BG": "{:set_case:lower}{:set_space:_}",
-	// "SKS" -> "SCREAMING_SNAKE_CASE"
-	"S-BGS": "{:set_case:lower}{:set_space:_}",
-	// "KK" -> "kebab-case"
-	"K-BG": "{:set_case:lower}{:set_space:-}",
-}
-await fs.writeFile('benno-steno-dicts/casing.json', JSON.stringify(casing, null, '\t'), 'utf-8')
+  // "NK" -> "normal case"
+  "TPH-BG": "{:set_space: }{:set_case:normal}",
+  // "TK" -> "Title Case"
+  "T-BG": "{:set_case:title}{:set_space:}",
+  // "PK" -> "PascalCase"
+  "P-BG": "{:set_case:title}{:set_space:}",
+  // "CK" -> "camelCase"
+  "KR-BG": "{:set_case:title}{:set_space:}{>}",
+  // "SK" -> "snake_case"
+  "S-BG": "{:set_case:lower}{:set_space:_}",
+  // "SKS" -> "SCREAMING_SNAKE_CASE"
+  "S-BGS": "{:set_case:lower}{:set_space:_}",
+  // "KK" -> "kebab-case"
+  "K-BG": "{:set_case:lower}{:set_space:-}",
+};
+await fs.writeFile(
+  "benno-steno-dicts/casing.json",
+  JSON.stringify(casing, null, "\t"),
+  "utf-8",
+);
 
-await fs.writeFile('benno-02-steno-dicts.json', JSON.stringify({ ...movement3, ...casing }, null, '\t'), 'utf-8')
+await fs.writeFile(
+  "benno-02-steno-dicts.json",
+  JSON.stringify({ ...movement3, ...casing }, null, "\t"),
+  "utf-8",
+);
